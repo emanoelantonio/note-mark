600d7eb020bb18d1ff21e4a23559647f794c7620
emanoelantonio <dev.emanoel@protonmail.com>
2024-02-06 20:35:20 -0300

    feat:  prepare interface

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,16 @@
-function App(): JSX.Element {
+import { ActionButtonsRow, Content, DraggableTopBar, RootLayout, Sidebar } from '@/components'
+
+const App = () => {
   return (
-    <div className="flex h-full items-center justify-center">
-      <span className="text-4xl text-blue-500">This is an Electron App</span>
-    </div>
+    <>
+      <DraggableTopBar />
+      <RootLayout>
+        <Sidebar className="p-2">
+          <ActionButtonsRow className="flex justify-between mt-1" />
+        </Sidebar>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+      </RootLayout>
+    </>
   )
 }
 
