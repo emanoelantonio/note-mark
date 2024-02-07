a6e0479e4cb8127e3a2b029d53813407345a7b2b
emanoelantonio <dev.emanoel@protonmail.com>
2024-02-07 18:04:49 -0300

    feat: add markdown editor component, scroolbar and Floating Title

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -6,7 +6,7 @@ if (!process.contextIsolated) {
 
 try {
   contextBridge.exposeInMainWorld('context', {
-    //TODO:
+    location: navigator.language
   })
 } catch (error) {
   console.log(error);
