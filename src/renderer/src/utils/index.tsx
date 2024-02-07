0cbc2d46fbc5be3f45759af40ac1ef1e28186415
emanoelantonio <dev.emanoel@protonmail.com>
2024-02-06 22:04:05 -0300

    feat: add type models, mocks and notes

--- src/renderer/src/utils/index.tsx
+++ src/renderer/src/utils/index.tsx
@@ -1,6 +1,13 @@
 import clsx, { ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+const dateFormatter = new Intl.DateTimeFormat('pt-BR', {
+  dateStyle: 'medium',
+  timeStyle: 'short'
+})
+
+export const formatDateFromMs = (ms: number) => dateFormatter.format(ms)
+
 export const cn = (...args: ClassValue[]) => {
   return twMerge(clsx(...args))
 }
